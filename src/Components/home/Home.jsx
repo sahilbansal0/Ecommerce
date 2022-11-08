6b4d767680459a5d7359d6ff1acd6cafb3a50111
sahil <skb.bansal786@gmail.com>
2022-11-08 21:56:08 +0530

    ss

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -2,29 +2,28 @@ import React from 'react'
 import Allcategories from './Allcategories'
 import Singlecateg from './Singlecateg'
 // import Carousel from './Carousel'
-import Electronics from '../products/Electronics'
 import Navbar from '../Navbar'
 
 const Home = () => {
   console.log('HomePage-->>')
   const [flag, setFlag] = React.useState(false);
-    let d = JSON.parse(localStorage.getItem("token"));
-    React.useEffect(() => {
-      console.log('dddd',d)
-      setFlag(d)
-    }, []);
+  let d = JSON.parse(localStorage.getItem("token"));
+  React.useEffect(() => {
+    console.log('dddd', d)
+    setFlag(d)
+  }, []);
 
   return (
     <div>
       {/* <h1>home</h1> */}
-      <Navbar flag={flag}/>
+      <Navbar flag={flag} />
 
-      <Allcategories/>
-      <Singlecateg/>
-      
+      <Allcategories />
+      <Singlecateg />
+
       {/* <Carousel/>       */}
-      
-      
+
+
     </div>
   )
 }
